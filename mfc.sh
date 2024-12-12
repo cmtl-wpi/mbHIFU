c0ac1bf240b353457d032c1e8ddf72988d02cd6f
Diego Vaca <dgvacarevelo@login-03.turing.wpi.edu>
2024-12-12 11:31:21 -0500

    HIFU with GPUs starting point

--- mfc.sh
+++ mfc.sh
@@ -35,7 +35,7 @@ elif [ "$1" '==' "format" ]; then
 elif [ "$1" '==' "venv" ]; then
     shift; . "$(pwd)/toolchain/bootstrap/python.sh"  $@; return
 elif [ "$1" '==' "clean" ]; then
-    rm -rf "$(pwd)/build/staging/"; exit 0
+    rm -rf "$(pwd)/build"; exit 0
 elif [ "$1" '==' "spelling" ]; then
     . "$(pwd)/toolchain/bootstrap/python.sh"
 
@@ -46,6 +46,10 @@ mkdir -p "$(pwd)/build"
 
 . "$(pwd)/toolchain/bootstrap/cmake.sh"
 . "$(pwd)/toolchain/bootstrap/python.sh"
+
+if [ "$1" '==' 'init' ]; then
+    exit 0
+fi
 
 echo
 
